8e94b6379fd1ebc233be2fdcb2d2d2e75ae08bdb
firy <l.ciniello@studenti.unisa.it>
2025-06-05 00:57:12 +0200

    Eliminato QuestionDAO e modificati leggermente Question e QuestionType

--- src/main/java/quiz/QuestionType.java
+++ src/main/java/quiz/QuestionType.java
@@ -1,31 +1,45 @@
 package quiz;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public enum QuestionType {
     ABSOLUTE_FREQUENCY("Frequenza Assoluta",
-            "Quante volte compare la parola '%s' %s?"),
+            Arrays.asList(
+                    "Quante volte compare la parola '%s' nel documento %s?",
+                    "Qual è la frequenza assoluta della parola '%s' in %s?",
+                    "Quante occorrenze ha la parola '%s' nel testo %s?")),
 
     COMPARISON("Confronto",
-            "Quale tra le seguenti parole è la più frequente %s?"),
+            Arrays.asList(
+                    "Quale tra le seguenti parole è la più frequente %s?",
+                    "Tra queste parole, quale appare più spesso %s?")),
 
     DOCUMENT_SPECIFIC("Documento Specifico",
-            "In quale documento compare più spesso la parola '%s'?"),
+            Arrays.asList(
+                    "In quale documento compare più spesso la parola '%s'?",
+                    "Dove appare più frequentemente la parola '%s'?")),
 
     EXCLUSION("Esclusione",
-            "Quale di queste parole NON compare mai %s?");
+            Arrays.asList(
+                    "Quale di queste parole NON compare mai %s?",
+                    "Quale tra queste parole è assente %s?"));
 
-    private final String displayName;
-    private final String questionTemplate;
+    private final String type;
+    private final List<String> questionList;
 
-    QuestionType(String displayName, String questionTemplate) {
-        this.displayName = displayName;
-        this.questionTemplate = questionTemplate;
+    QuestionType(String type, List<String> questionList) {
+        this.type = type;
+        this.questionList = questionList;
     }
 
-    public String getDisplayName() {
-        return displayName;
+    public String getType() {
+        return type;
     }
 
-    public String getQuestionTemplate() {
-        return questionTemplate;
+    public List<String> getQuestionList() {
+        return questionList;
     }
+
 }
