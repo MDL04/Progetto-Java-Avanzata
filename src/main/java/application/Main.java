c17301a9c88136c5b798369993fc5d5043f93d8f
MDL04 <m.dilieto12@studenti.unisa.it>
2025-06-09 17:40:06 +0200

    Base code

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Registrazione");
+        stage.setTitle("Homepage");
         stage.setScene(scene);
         stage.show();
     }
